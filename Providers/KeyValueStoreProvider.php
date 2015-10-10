faa3055ccdec8cbfb370d49ce488d6fb954494c1
SAEED AZIZI <saeedazizi1369@yahoo.com>
2015-10-10 17:37:40 +0330

    edit provider

--- Providers/KeyValueStoreProvider.php
+++ Providers/KeyValueStoreProvider.php
@@ -11,10 +11,6 @@ class KeyValueStoreProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            dirname(__DIR__) . '/config/key-value-store.php' => config_path('key-value-store.php'),
-        ], 'public');
-
-        $this->publishes([
             dirname(__DIR__) . '/migration' => database_path('migrations'),
         ], 'public');
     }
